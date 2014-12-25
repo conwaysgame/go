8d82f81960f5d5a7df007bc60000a6c686b3cf2d
Dan Hough <daniel.hough@gmail.com>
2014-12-25 16:13:33 -0400

    Checking for live neighbours

--- gameOfLife.go
+++ gameOfLife.go
@@ -1,13 +1,18 @@
 package gameOfLife
 
+import (
+  "math"
+  // "fmt"
+)
+
 var world [5][5]bool
 
 func ResetWorld() {
-		for x := 0; x < 5; x++ {
-			for y := 0; y < 5; y++ {
-			  world[x][y] = false
-			}
-		}
+  for x := 0; x < 5; x++ {
+    for y := 0; y < 5; y++ {
+      world[x][y] = false
+    }
+  }
 }
 
 func Populate(x int, y int) {
@@ -15,10 +20,26 @@ func Populate(x int, y int) {
 }
 
 func Toggle(x int, y int) {
-	world[x][y] = !world[x][y]
+  world[x][y] = !world[x][y]
 }
 
-func Living (x int, y int) bool {
+func NumberOfLivingNeighbours(x int, y int) int {
+  var neighbours int = 0
+  // fmt.Printf("For %d, %d\n", x, y)
+  for nx := int(math.Max(float64(x - 1), 0)); nx < int(math.Min(float64(x + 2), 5)); nx++ {
+    for ny := int(math.Max(float64(y - 1), 0)); ny < int(math.Min(float64(y + 2), 5)); ny++ {
+      // fmt.Printf("Checking %d, %d...", nx, ny)
+      if ((nx != x || ny != y) && Living(nx, ny)) {
+        // fmt.Printf("Yup!")
+        neighbours++
+      }
+      // fmt.Printf("\n")
+    }
+  }
+  return neighbours
+}
+
+func Living(x int, y int) bool {
   return world[x][y]
 }
 
--- gameOfLife_test.go
+++ gameOfLife_test.go
@@ -18,3 +18,29 @@ func TestToggle(t *testing.T) {
   assert.Equal(t, Living(0, 2), true)
   assert.Equal(t, Living(0, 1), false)
 }
+
+// OOOOO
+// OOOOO
+// XXOOO
+// XOOOO
+// OOOOO
+// 
+
+func TestNumberOfLivingNeighbours(t *testing.T) {
+	ResetWorld()
+  Toggle(0, 2)
+  Toggle(0, 3)
+  Toggle(1, 2)
+  assert.Equal(t, NumberOfLivingNeighbours(1, 3), 3)
+  assert.Equal(t, NumberOfLivingNeighbours(1, 1), 2)
+  assert.Equal(t, NumberOfLivingNeighbours(2, 2), 1)
+  assert.Equal(t, NumberOfLivingNeighbours(0, 0), 0)
+}
+
+func TestNumberOfLivingNeighboursForLiveCell(t *testing.T) {
+	ResetWorld()
+  Toggle(0, 2)
+  Toggle(0, 3)
+  Toggle(1, 2)
+  assert.Equal(t, NumberOfLivingNeighbours(0, 2), 2)
+}
